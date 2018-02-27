8cd5874f6714c553f52366f82f9f00373c7d11c0
gdyaojinhong <gdyaojinhong@163.com>
2018-02-27 18:05:34 +0800

    新增淘宝rem

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import axios from 'axios'
+import 'lib-flexible'
 
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
